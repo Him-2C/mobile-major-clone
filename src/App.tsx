1fd7d087d100238171f26ad8591001f598844063
Chakkrit Chueangyang <Him.Chakkrit@gmail.com>
2023-06-11 11:59:28 +0700

    Feat: add emotion style

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from "@emotion/native";
+import { StatusBar } from "expo-status-bar";
+import { Text } from "react-native";
 
-export default function App() {
+const Container = styled.View({
+  flex: 1,
+  backgroundColor: "#fff",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const App = () => {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
